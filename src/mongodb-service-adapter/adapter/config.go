250b33b8f6ef063aa43b6e5d2370d88a7977cb49
Ramin Taghizada <ramintagizade@mail.ru>
2019-03-29 11:26:51 +0300

    add adapter config file

---
+++ src/mongodb-service-adapter/adapter/config.go
@@ -0,0 +1,70 @@
+package adapter
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+type Config struct {
+	URL           string `json:"url"`
+	Username      string `json:"username"`
+	APIKey        string `json:"api_key"`
+	AuthKey       string `json:"auth_key"`
+	GroupID       string `json:"group"`
+	NodeAddresses string `json:"nodes"`
+}
+
+func LoadConfig(configFile string) (config *Config, err error) {
+	if configFile == "" {
+		return config, errors.New("Must provide a config file")
+	}
+
+	file, err := os.Open(configFile)
+	if err != nil {
+		return config, err
+	}
+	defer file.Close()
+
+	bytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		return config, err
+	}
+
+	if err = json.Unmarshal(bytes, &config); err != nil {
+		return config, err
+	}
+
+	if err = config.Validate(); err != nil {
+		return config, fmt.Errorf("Validating config contents: %s", err)
+	}
+
+	return config, nil
+}
+
+func (c Config) Validate() error {
+
+	if c.URL == "" {
+		return errors.New("Must provide a non-empty URL")
+	}
+
+	if c.Username == "" {
+		return errors.New("Must provide a non-empty Username")
+	}
+
+	if c.APIKey == "" {
+		return errors.New("Must provide a non-empty API Key")
+	}
+
+	if c.GroupID == "" {
+		return errors.New("Must provide a non-empty Group ID")
+	}
+
+	if c.NodeAddresses == "" {
+		return errors.New("Must provide a non-empty Node Addresses")
+	}
+
+	return nil
+}
