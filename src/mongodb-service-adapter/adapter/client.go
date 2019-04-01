ebf201288998973d246c5f9a03738f23fa1ffeb6
Andrey Belik <belika@hotmail.com>
2019-04-01 16:23:13 +0100

    fixing error for Cloud manager permission

--- src/mongodb-service-adapter/adapter/client.go
+++ src/mongodb-service-adapter/adapter/client.go
@@ -105,7 +105,7 @@ func (oc *OMClient) LoadDoc(p string, ctx *DocContext) (string, error) {
 func (oc *OMClient) GetGroupByName(name string) (Group, error) {
 	var group Group
 	b, err := oc.doRequest("GET", fmt.Sprintf("/api/public/v1.0/groups/byName/%s", name), nil)
-	if err != nil {
+	if err != nil && !strings.Contains(strings.ToLower(err.Error()), "not_in_group") && !strings.Contains(string(b), "NOT_IN_GROUP") {
 		return group, err
 	}
 	if err = json.Unmarshal(b, &group); err != nil {
