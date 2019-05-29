45938b6518bfe799846c252f57622a4283c9a1b1
Andrey Belik <belika@hotmail.com>
2019-05-29 23:15:53 +0100

    fixing Group object

--- src/mongodb-service-adapter/adapter/client.go
+++ src/mongodb-service-adapter/adapter/client.go
@@ -116,7 +116,7 @@ func (oc *OMClient) GetGroupByName(name string) (Group, error) {
 		fmt.Println("Client GetGroupByName json.Unmarshal error: ", err)
 		return group, err
 	}
-	fmt.Println("Client GetGroupByName parsed group ", group.name)
+	fmt.Println("Client GetGroupByName parsed group ", group.Name)
 	return group, nil
 }
 
@@ -138,7 +138,7 @@ func (oc *OMClient) CreateGroup(id string, request GroupCreateRequest) (Group, e
 		log.Println(fmt.Sprintf("CreateGroup GetGroupByName with request.Name : %s ,error: %v", request.Name, err))
 		return group, err
 	}
-	if group.name == request.Name {
+	if group.Name == request.Name {
 		log.Printf("Continue with existing group %q", group.ID)
 		apiKey, err := oc.CreateGroupAPIKey(group.ID)
 		if err != nil {
