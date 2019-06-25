c0cd6f57001af3856edfe2efd0af3b2c85a0bd60
Pavel Vasilev <explosere@gmail.com>
2019-06-25 18:34:28 +0300

    Add a workaround for CTFILE-25; source code cleanup

--- src/mongodb-service-adapter/main.go
+++ src/mongodb-service-adapter/main.go
@@ -4,7 +4,8 @@ import (
 	"log"
 	"os"
 
-	"github.com/10gen/ops-manager-cloudfoundry/src/mongodb-service-adapter/adapter"
+	"mongodb-service-adapter/adapter"
+
 	"github.com/pivotal-cf/on-demand-services-sdk/serviceadapter"
 )
 
