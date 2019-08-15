32c0e985e5680104564492f84c57c2972d222aef
Pavel Vasilev <explosere@gmail.com>
2019-08-15 17:07:54 +0300

    Use every possible method from pcgc in client.go

--- src/mongodb-service-adapter/adapter/client.go
+++ src/mongodb-service-adapter/adapter/client.go
@@ -12,6 +12,9 @@ import (
 	"strings"
 
 	"mongodb-service-adapter/digest"
+
+	"github.com/mongodb-labs/pcgc/pkg/httpclient"
+	"github.com/mongodb-labs/pcgc/pkg/opsmanager"
 	"github.com/tidwall/gjson"
 )
 
@@ -104,78 +107,59 @@ func (oc *OMClient) LoadDoc(p string, ctx *DocContext) (string, error) {
 
 //GetGroupByName returns group if found.
 func (oc *OMClient) GetGroupByName(name string) (Group, error) {
-	var group Group
-	b, err := oc.doRequest("GET", fmt.Sprintf("/api/public/v1.0/groups/byName/%s", name), nil)
-
-	if err != nil {
-		log.Println("Client GetGroupByName "+fmt.Sprintf(" oc.doRequest GET/api/public/v1.0/groups/byName/%s , error:: ", name), err)
-		return group, err
-	}
-	if err = json.Unmarshal(b, &group); err != nil {
-		fmt.Println("Client GetGroupByName json.Unmarshal error: ", err)
-		return group, err
-	}
-	return group, nil
+	newc := opsmanager.NewClientWithDigestAuth(httpclient.NewURLResolver(oc.Url), oc.Username, oc.ApiKey)
+	result, err := newc.GetProjectByName(name)
+	return Group(result), err
 }
 
+// TODO: implement in client
 func (oc *OMClient) CreateGroup(id string, request GroupCreateRequest) (Group, error) {
-	var group Group
-
 	log.Println(fmt.Sprintf("CreateGroup in id : %s ,request : %+v", id, request))
+	newc := opsmanager.NewClientWithDigestAuth(httpclient.NewURLResolver(oc.Url), oc.Username, oc.ApiKey)
+
 	if request.Name == "" {
 		request.Name = fmt.Sprintf("PCF_%s", strings.ToUpper(id))
 	}
-	req, err := json.Marshal(request)
+
+	group, err := oc.GetGroupByName(request.Name)
 	if err != nil {
-		log.Println(fmt.Sprintf("CreateGroup json.Marshal with data: %+v error: %v", request, err))
+		log.Printf("CreateGroup GetGroupByName with request.Name: %s, error: %v", request.Name, err)
 		return group, err
 	}
 
-	group, err = oc.GetGroupByName(request.Name)
-	if err != nil {
-		log.Println(fmt.Sprintf("CreateGroup GetGroupByName with request.Name : %s ,error: %v", request.Name, err))
-		return group, err
-	}
 	if group.Name == request.Name {
 		log.Printf("Continue with existing group %q", group.ID)
 		apiKey, err := oc.CreateGroupAPIKey(group.ID)
 		if err != nil {
-			log.Println(fmt.Sprintf("CreateGroup CreateGroupAPIKey group.ID : %s ,error: %v", group.ID, err))
+			log.Printf("CreateGroup CreateGroupAPIKey group.ID: %s, error: %v", group.ID, err)
 			return group, err
 		}
 		group.AgentAPIKey = apiKey
 		log.Println()
 		return group, nil
 	}
-	b, err := oc.doRequest("POST", "/api/public/v1.0/groups", bytes.NewReader(req))
+
+	resp, err := newc.CreateOneProject(request.Name, request.OrgId)
 	if err != nil {
-		log.Println(fmt.Sprintf("CreateGroup oc.doRequest /api/public/v1.0/groups, body: %s error: %v", req, err))
+		log.Printf("CreateGroup CreateOneProject, request: %+v, error: %v", request, err)
 		return group, err
 	}
 
-	if err = json.Unmarshal(b, &group); err != nil {
-		fmt.Println("Client CreateGroup json.Unmarshal error: ", err)
-		return group, err
-	}
-	return group, nil
+	return Group(resp.Project), nil
 }
 
 func (oc *OMClient) CreateGroupAPIKey(groupID string) (string, error) {
-	desc := `{"desc": "MongoDB On-Demand broker generated Agent API Key"}`
+	newc := opsmanager.NewClientWithDigestAuth(httpclient.NewURLResolver(oc.Url), oc.Username, oc.ApiKey)
 
-	b, err := oc.doRequest("POST", fmt.Sprintf("/api/public/v1.0/groups/%s/agentapikeys", groupID), strings.NewReader(desc))
+	result, err := newc.CreateAgentAPIKEY(groupID, "MongoDB On-Demand broker generated Agent API Key")
 	if err != nil {
-		return "", fmt.Errorf("unable to create agent api key for group %q", groupID)
+		return "", fmt.Errorf("unable to create agent api key for group %s: %v", groupID, err)
 	}
 
-	key := gjson.GetBytes(b, "key")
-	if key.String() == "" {
-		return "", errors.New("Failed to create agent api key for group " + groupID)
-	}
-
-	return key.String(), nil
+	return result.Key, nil
 }
 
+// TODO: implement in client
 func (oc *OMClient) UpdateGroup(id string, request GroupUpdateRequest) (Group, error) {
 	var group Group
 
@@ -196,14 +180,20 @@ func (oc *OMClient) UpdateGroup(id string, request GroupUpdateRequest) (Group, e
 }
 
 func (oc *OMClient) GetGroupAuthAgentPassword(groupID string) (string, error) {
-	b, err := oc.doRequest("GET", fmt.Sprintf("/api/public/v1.0/groups/%s/automationConfig", groupID), nil)
+	newc := opsmanager.NewClientWithDigestAuth(httpclient.NewURLResolver(oc.Url), oc.Username, oc.ApiKey)
+	config, err := newc.GetAutomationConfig(groupID)
 	if err != nil {
 		return "", err
 	}
-	authPwd := gjson.GetBytes(b, "auth.autoPwd")
-	return authPwd.String(), nil
+
+	result, ok := config.Auth["autoPwd"].(string)
+	if !ok {
+		return "", errors.New("'autoPwd' is not a string")
+	}
+	return result, nil
 }
 
+// TODO: implement in client
 func (oc *OMClient) GetGroup(groupID string) (Group, error) {
 	var group Group
 
@@ -219,11 +209,13 @@ func (oc *OMClient) GetGroup(groupID string) (Group, error) {
 	return group, nil
 }
 
+// TODO: implement in client
 func (oc *OMClient) DeleteGroup(groupID string) error {
 	_, err := oc.doRequest("DELETE", fmt.Sprintf("/api/public/v1.0/groups/%s", groupID), nil)
 	return err
 }
 
+// TODO: implement in client
 func (oc *OMClient) GetGroupHosts(groupID string) (GroupHosts, error) {
 	var groupHosts GroupHosts
 
@@ -239,6 +231,7 @@ func (oc *OMClient) GetGroupHosts(groupID string) (GroupHosts, error) {
 	return groupHosts, nil
 }
 
+// TODO: implement in client
 func (oc *OMClient) GetGroupHostnames(groupID string, planID string) ([]string, error) {
 	b, err := oc.doRequest("GET", fmt.Sprintf("/api/public/v1.0/groups/%s/hosts", groupID), nil)
 	if err != nil {
@@ -260,28 +253,40 @@ func (oc *OMClient) GetGroupHostnames(groupID string, planID string) ([]string, 
 
 // TODO: refactor the client to avoid this call
 func (oc *OMClient) GetKeyfileWindows(groupID string) (string, error) {
-	u := fmt.Sprintf("/api/public/v1.0/groups/%s/automationConfig", groupID)
-	b, err := oc.doRequest(http.MethodGet, u, nil)
+	newc := opsmanager.NewClientWithDigestAuth(httpclient.NewURLResolver(oc.Url), oc.Username, oc.ApiKey)
+	config, err := newc.GetAutomationConfig(groupID)
 	if err != nil {
 		return "", err
 	}
-	log.Println(string(b))
 
-	kfw := gjson.GetBytes(b, "auth.keyfileWindows")
-	return kfw.String(), nil
+	result, ok := config.Auth["keyfileWindows"].(string)
+	if !ok {
+		return "", nil // no keyfileWindows is OK
+	}
+
+	return result, nil
 }
 
 func (oc *OMClient) ConfigureGroup(configurationDoc string, groupID string) error {
-	u := fmt.Sprintf("/api/public/v1.0/groups/%s/automationConfig", groupID)
-	b, err := oc.doRequest("PUT", u, strings.NewReader(configurationDoc))
+	config := opsmanager.AutomationConfig{}
+	err := json.Unmarshal([]byte(configurationDoc), &config)
 	if err != nil {
 		return err
 	}
+
+	newc := opsmanager.NewClientWithDigestAuth(httpclient.NewURLResolver(oc.Url), oc.Username, oc.ApiKey)
+	config, err = newc.UpdateAutomationConfig(groupID, config)
+	if err != nil {
+		return err
+	}
+
+	b, _ := json.Marshal(config)
 	log.Println(string(b))
 
 	return nil
 }
 
+// TODO: implement in client
 func (oc *OMClient) ConfigureMonitoringAgent(configurationDoc string, groupID string) error {
 	u := fmt.Sprintf("/api/public/v1.0/groups/%s/automationConfig/monitoringAgentConfig", groupID)
 	b, err := oc.doRequest("PUT", u, strings.NewReader(configurationDoc))
@@ -293,6 +298,7 @@ func (oc *OMClient) ConfigureMonitoringAgent(configurationDoc string, groupID st
 	return nil
 }
 
+// TODO: implement in client
 func (oc *OMClient) ConfigureBackupAgent(configurationDoc string, groupID string) error {
 	u := fmt.Sprintf("/api/public/v1.0/groups/%s/automationConfig/backupAgentConfig", groupID)
 	b, err := oc.doRequest("PUT", u, strings.NewReader(configurationDoc))
@@ -303,34 +309,30 @@ func (oc *OMClient) ConfigureBackupAgent(configurationDoc string, groupID string
 
 	return nil
 }
-func (oc *OMClient) GetAvailableVersions(groupID string) (Automation, error) {
-	var versions Automation
 
-	b, err := oc.doRequest("GET", fmt.Sprintf("/api/public/v1.0/groups/%s/automationConfig", groupID), nil)
+func (oc *OMClient) GetAvailableVersions(groupID string) ([]*opsmanager.MongoDBVersion, error) {
+	newc := opsmanager.NewClientWithDigestAuth(httpclient.NewURLResolver(oc.Url), oc.Username, oc.ApiKey)
+	config, err := newc.GetAutomationConfig(groupID)
 	if err != nil {
-		return versions, err
+		return nil, err
 	}
 
-	if err = json.Unmarshal(b, &versions); err != nil {
-		fmt.Println("Client GetAvailableVersions json.Unmarshal error: ", err)
-		return versions, err
-	}
-	return versions, nil
+	return config.MongoDBVersions, nil
 }
 
 func (oc *OMClient) GetLatestVersion(groupID string) (string, error) {
-	cfg, err := oc.GetAvailableVersions(groupID)
+	result, err := oc.GetAvailableVersions(groupID)
 	if err != nil {
 		return "", fmt.Errorf("unable to find the latest MongoDB version from the MongoDB Ops Manager API. Please contact your system administrator to ensure versions are available in the Version Manager for group '%q' in MongoDB Ops Manager. If your MongoDB Ops Manager is running in Local Mode, then after validating versions are available, please indicate a specific MongoDB version using 'version’ paramater when calling 'create-service'", groupID)
 	}
 
-	if len(cfg.MongoDbVersions) == 0 {
+	if len(result) == 0 {
 		return "", fmt.Errorf("unable to find the latest MongoDB version from the MongoDB Ops Manager API. Please contact your system administrator to ensure versions are available in the Version Manager for group '%q' in MongoDB Ops Manager. If your MongoDB Ops Manager is running in Local Mode, then after validating versions are available, please indicate a specific MongoDB version using 'version’ paramater when calling 'create-service'", groupID)
 	}
 
-	versions := make([]string, len(cfg.MongoDbVersions))
+	versions := make([]string, len(result))
 	n := 0
-	for _, i := range cfg.MongoDbVersions {
+	for _, i := range result {
 		if !strings.HasSuffix(i.Name, "ent") {
 			versions[n] = i.Name
 			n++
@@ -343,18 +345,18 @@ func (oc *OMClient) GetLatestVersion(groupID string) (string, error) {
 }
 
 func (oc *OMClient) ValidateVersion(groupID string, version string) (string, error) {
-	b, err := oc.doRequest("GET", fmt.Sprintf("/api/public/v1.0/groups/%s/automationConfig", groupID), nil)
+	result, err := oc.GetAvailableVersions(groupID)
 	if err != nil {
 		return "", err
 	}
 
-	v := gjson.GetBytes(b, fmt.Sprintf(`mongoDbVersions.#[name="%s"].name`, version))
-	log.Printf("Using %q version of MongoDB", v.String())
-	if v.String() == "" {
-		return "", errors.New("failed to find expected version, got " + version)
+	for _, v := range result {
+		if v.Name == version {
+			return version, nil
+		}
 	}
 
-	return v.String(), nil
+	return "", errors.New("failed to find expected version, got " + version)
 }
 
 func (oc *OMClient) ValidateVersionManifest(version string) (string, error) {
