767476ca3166df219668ddf7e4954842c5a4cb84
Leo <5437289+cross44@users.noreply.github.com>
2019-09-27 17:05:30 +0300

    updated config

--- ci/tasks/deploy-mongodb-service/run.sh
+++ ci/tasks/deploy-mongodb-service/run.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 [ 'true' = "${DEBUG:-}" ] && set -x
 
-cf create-service mongodb-odb replica_set_small mongodb-service-instance -c '{ "backup_enabled" : "true"}'
+cf create-service mongodb-odb replica_set_small mongodb-service-instance -c '{"backup_enabled":"true"}'
 cf push app-mongodb-on-demand
 cf bind-service app-mongodb-on-demand mongodb-service-instance --binding-name mongodb-service
 cf restage app-mongodb-on-demand
