ac32f401618cdee79eb4d9574180a07f5300a04c
Leo <5437289+cross44@users.noreply.github.com>
2019-09-30 15:45:40 +0300

    params for cf login

--- ci/tasks/deploy-mongodb-service/run.sh
+++ ci/tasks/deploy-mongodb-service/run.sh
@@ -1,8 +1,8 @@
 #!/usr/local/bin/dumb-init /bin/bash
 set -euo pipefail
 [ 'true' = "${DEBUG:-}" ] && set -x
-
-cf create-service mongodb-odb replica_set_small mongodb-service-instance -c '{"backup_enabled":"true"}'
+cf login -a $CF_APP_URL -u $CF_APP_USER -p $CF_APP_PASSWORD --skip-ssl-validation -o system -s system
+cf create-service mongodb-odb $REPLICA_SET_PLAN mongodb-service-instance -c "{\"backup_enabled\":\"$CONFIG\"}"
 cf push app-mongodb-on-demand
 cf bind-service app-mongodb-on-demand mongodb-service-instance --binding-name mongodb-service
 cf restage app-mongodb-on-demand
