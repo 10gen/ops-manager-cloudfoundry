1264346f40baaa22702c077d4bbdcec090413d33
Leo <5437289+cross44@users.noreply.github.com>
2019-09-30 17:19:21 +0300

    split job in 2:deploy-old-tile+deploy-service

--- ci/tasks/deploy-mongodb-service/run.sh
+++ ci/tasks/deploy-mongodb-service/run.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 [ 'true' = "${DEBUG:-}" ] && set -x
 cf login -a $CF_APP_URL -u $CF_APP_USER -p $CF_APP_PASSWORD --skip-ssl-validation -o system -s system
-cf create-service mongodb-odb $REPLICA_SET_PLAN mongodb-service-instance -c "{\"backup_enabled\":\"$CONFIG\"}"
+cf create-service mongodb-odb $REPLICA_SET_PLAN mongodb-service-instance -c "{\"backup_enabled\":\"$BACKUP_ENABLED\"}"
 cf push app-mongodb-on-demand
 cf bind-service app-mongodb-on-demand mongodb-service-instance --binding-name mongodb-service
 cf restage app-mongodb-on-demand
