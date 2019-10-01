5e1a95fb52f4e734564a99693931bed72036935b
Leo <5437289+cross44@users.noreply.github.com>
2019-10-01 15:12:11 +0300

    some fixes:new om url, add app params

--- ci/tasks/deploy-mongodb-service/run.sh
+++ ci/tasks/deploy-mongodb-service/run.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 [ 'true' = "${DEBUG:-}" ] && set -x
 cf login -a $CF_APP_URL -u $CF_APP_USER -p $CF_APP_PASSWORD --skip-ssl-validation -o system -s system
-cf create-service mongodb-odb $REPLICA_SET_PLAN mongodb-service-instance -c "{\"backup_enabled\":\"$BACKUP_ENABLED\"}"
-cf push app-mongodb-on-demand
-cf bind-service app-mongodb-on-demand mongodb-service-instance --binding-name mongodb-service
-cf restage app-mongodb-on-demand
+cf create-service mongodb-odb $REPLICA_SET_PLAN mongodb-service-instance -c "{\"enable_backup\":\"$BACKUP_ENABLED\"}"
+cf push app-ruby-sample -p $base/ops-manager-cloudfoundry/src/smoke-tests/assets/cf-mongo-example-app
+cf bind-service app-ruby-sample mongodb-service-instance --binding-name mongodb-service
+cf restage app-ruby-sample
