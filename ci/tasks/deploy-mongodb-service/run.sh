1b1a6244cb2134b34a6ca9bb26c9cc6150db0c65
Leo <5437289+cross44@users.noreply.github.com>
2019-10-02 11:36:16 +0300

    fixes(ci):

--- ci/tasks/deploy-mongodb-service/run.sh
+++ ci/tasks/deploy-mongodb-service/run.sh
@@ -1,6 +1,8 @@
 #!/usr/local/bin/dumb-init /bin/bash
 set -euo pipefail
 [ 'true' = "${DEBUG:-}" ] && set -x
+base=$PWD
+
 cf login -a $CF_APP_URL -u $CF_APP_USER -p $CF_APP_PASSWORD --skip-ssl-validation -o system -s system
 cf create-service mongodb-odb $REPLICA_SET_PLAN mongodb-service-instance -c "{\"enable_backup\":\"$BACKUP_ENABLED\"}"
 cf push app-ruby-sample -p $base/ops-manager-cloudfoundry/src/smoke-tests/assets/cf-mongo-example-app
