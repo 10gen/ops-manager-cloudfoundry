2f864f523e6125c0f0a3b1851ec1c07c2a32369a
Leo <5437289+cross44@users.noreply.github.com>
2019-10-03 19:55:32 +0300

    for some test

--- ci/tasks/application-check/run-get.sh
+++ ci/tasks/application-check/run-get.sh
@@ -8,6 +8,10 @@ end-point="http://{$host}/service/mongo/test3"
 result=$(echo $(curl -X GET -H "Content-Type: application/json" ${end-point}))
 if [ "${result}" = '{"data":"sometest130"}' ]; then
         echo "Application is working"
+        curl -X DELETE -H "Content-Type: application/json" ${end-point}
+        cf unbind-service app-ruby-sample mongodb-service-instance
+        cf delete-service mongodb-service-instance -f
+        cf delete app-ruby-sample -f
     else
         echo "GET ${end-point} = ${result}"
         echo "FAILED. Application doesn't work"
