2bda5c6155704bb853e08b788c0ed6a6861eb1c7
Leo <5437289+cross44@users.noreply.github.com>
2019-10-08 20:27:47 +0300

    fix [[

--- ci/tasks/deploy-mongodb-service/run-create-service.sh
+++ ci/tasks/deploy-mongodb-service/run-create-service.sh
@@ -7,13 +7,13 @@ cf login -a $CF_APP_URL -u $CF_APP_USER -p $CF_APP_PASSWORD --skip-ssl-validatio
 cf create-service mongodb-odb $REPLICA_SET_PLAN mongodb-service-instance -c "{\"enable_backup\":\"$BACKUP_ENABLED\"}"
 service_status=$(echo $(cf services | grep mongodb-service-instance | awk '{print $4" "$5" "$6}'))
 time=0
-until [[ ${service_status} != "create in progress" ]] || [[ $time > $INSTALL_TIMEOUT ]]; do
+until [[ $service_status != "create in progress" ]] || [[ $time -gt $INSTALL_TIMEOUT ]]; do
     echo "...${service_status}"
     sleep 3m
     let "time=$time+3"
     service_status=$(echo $(cf services | grep mongodb-service-instance | awk '{print $4" "$5" "$6}'))
 done
-if [ "${service_status}" = "create succeeded" ]; then
+if [[ $service_status = "create succeeded" ]]; then
     cf push app-ruby-sample -p $base/ops-manager-cloudfoundry/src/smoke-tests/assets/cf-mongo-example-app
     cf bind-service app-ruby-sample mongodb-service-instance --binding-name mongodb-service
     cf restage app-ruby-sample
