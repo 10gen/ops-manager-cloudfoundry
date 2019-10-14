217dae8473fd5542084e04503e72b9a8e785d47f
Leo <5437289+cross44@users.noreply.github.com>
2019-10-14 11:02:04 +0300

    change parameters name

--- ci/tasks/deploy-mongodb-service/run-create-service.sh
+++ ci/tasks/deploy-mongodb-service/run-create-service.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 base=$PWD
 
 cf login -a $CF_APP_URL -u $CF_APP_USER -p $CF_APP_PASSWORD --skip-ssl-validation -o system -s system
-cf create-service mongodb-odb $REPLICA_SET_PLAN mongodb-service-instance -c "{\"enable_backup\":\"$BACKUP_ENABLED\"}"
+cf create-service mongodb-odb $SET_PLAN mongodb-service-instance -c "{\"enable_backup\":\"$BACKUP_ENABLED\"}"
 service_status=$(echo $(cf services | grep mongodb-service-instance | awk '{print $4" "$5" "$6}'))
 time=0
 until [[ $service_status != "create in progress" ]] || [[ $time -gt $INSTALL_TIMEOUT ]]; do
