f72343ca42c8a306aaebe5869f89dfbdbbe932dd
Leo <5437289+cross44@users.noreply.github.com>
2019-11-14 15:20:29 +0300

    move config.pie## to creds

--- ci/tasks/deploy-tile/run.sh
+++ ci/tasks/deploy-tile/run.sh
@@ -56,8 +56,14 @@ OM_API_KEY: "$OM_API_KEY"
 OM_API_USER: "$OM_API_USER"
 
 EOF
+
 # ${om} configure-product --product-name "$PRODUCT" --product-network "$network_config" --product-properties "$properties_config"
-${om} configure-product  --config "$base/ops-manager-cloudfoundry/ci/tasks/deploy-tile/$CONFIG" -l "$base/ops-manager-cloudfoundry/ci/tasks/deploy-tile/vars.yml"
+config_path=$base/ops-manager-cloudfoundry/ci/tasks/deploy-tile/config.pie
+if [ -f $file ] ; then
+	rm $file
+fi
+echo "$CONFIG" >> config.pie
+${om} configure-product  --config "$config_path" -l "$base/ops-manager-cloudfoundry/ci/tasks/deploy-tile/vars.yml"
 
 STAGED=$(${om} curl --path /api/v0/staged/products)
 RESULT=$(echo "$STAGED" | jq --arg product_name "$PRODUCT" 'map(select(.type == $product_name)) | .[].guid')
