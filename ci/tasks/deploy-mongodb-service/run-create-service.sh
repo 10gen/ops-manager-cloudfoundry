688e27260f0534af90b75c9156e8088d606abd65
Leo <5437289+cross44@users.noreply.github.com>
2019-11-18 16:58:16 +0300

    ci(create service): delete service first

--- ci/tasks/deploy-mongodb-service/run-create-service.sh
+++ ci/tasks/deploy-mongodb-service/run-create-service.sh
@@ -6,8 +6,20 @@ base=$PWD
 cf login -a $CF_APP_URL -u $CF_APP_USER -p $CF_APP_PASSWORD --skip-ssl-validation -o system -s system
 service=$(cf services | awk '$1 ~ /test-mongodb-service/{print "exist"}')
 if [[ $service == "exist" ]]; then
-  cf purge-service-instance test-mongodb-service
+  cf delete-service test-mongodb-service -f
 fi
+time=0
+service_status=$(cf services | awk '$1 ~ /test-mongodb-service/{print $4" "$5" "$6}')
+until [[ $service_status != "delete in progress" ]] || [[ $time -gt $INSTALL_TIMEOUT ]]; do
+  echo "...${service_status}"
+  sleep 3m
+  let "time=$time+3"
+  service_status=$(cf services | awk '$1 ~ /test-mongodb-service/{print $4" "$5" "$6}') 
+done
+if [[ $service_status == "deleted failed" ]]; then
+  cf purge-service-instance test-mongodb-service -f
+fi
+
 cf create-service mongodb-odb "$SET_PLAN" test-mongodb-service -c "{\"enable_backup\":\"$BACKUP_ENABLED\"}"
 service_status=$(cf services | awk '$1 ~ /test-mongodb-service/{print $4" "$5" "$6}')
 time=0
