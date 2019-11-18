5e75c65058f8bf38cba30f0162f40f6716e1bf98
Leo <5437289+cross44@users.noreply.github.com>
2019-11-19 00:20:54 +0300

    ci(create): fix function parameters

--- ci/tasks/helpers/cf-helper.sh
+++ ci/tasks/helpers/cf-helper.sh
@@ -1,9 +1,11 @@
 #!/usr/local/bin/dumb-init /bin/bash
 
 #cf.helper. wait for particular service status 
-wait_service_status_change($instance_name, $status) {
+wait_service_status_change() {
+  local instance_name=$1
+  local status=$2
   local time=0
-  local verify_status=$(cf services | awk  '/$instance_name.*$status/{print "$status"}')
+  local verify_status=$(cf services | awk  '/'$instance_name'.*'$status'/{print "$status"}')
   until [[ $verify_status == $status ]] || [[ $time -gt $INSTALL_TIMEOUT ]]; do
     echo "...${verify_status}"
     sleep 3m
@@ -12,13 +14,14 @@ wait_service_status_change($instance_name, $status) {
   done
 }
 
-delete_service_if_exists($instance_name) {
-  local service=$(cf services | awk '$1 ~ /$instance_name/{print "exist"}')
+delete_service_if_exists() {
+  local instance_name=$1
+  local service=$(cf services | awk '/'$instance_name'/{print "exist"}')
   if [[ $service == "exist" ]]; then
     cf delete-service test-mongodb-service -f
   fi
   wait_service_status_change("delete in progress")
-  service_status=$(cf services | awk  '/test-mongodb-service.*failed/{print "failed"}')
+  service_status=$(cf services | awk  '/'$instance_name'.*failed/{print "failed"}')
   if [[ $service_status == "failed" ]]; then
     cf purge-service-instance test-mongodb-service -f
   fi
