1f633b4494b7f55eac64951ab6484dd5c5385c2d
Leo <5437289+cross44@users.noreply.github.com>
2019-12-10 16:30:48 +0300

    pcf param

--- ci/tasks/application-check/run-put.sh
+++ ci/tasks/application-check/run-put.sh
@@ -1,6 +1,7 @@
 #!/usr/local/bin/dumb-init /bin/bash
 set -eo pipefail
 [[ ${DEBUG:-} = true ]] && set -x
+base=$PWD
 . "$base/ops-manager-cloudfoundry/ci/tasks/helpers/cf-helper.sh"
 . "$base/ops-manager-cloudfoundry/ci/tasks/helpers/tmp-helper.sh"
 make_pcf_metadata
