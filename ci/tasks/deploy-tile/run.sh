a1e4247cff518922b9efa0571e01b709fe6ee0ef
Pavel Vasilev <explosere@gmail.com>
2019-12-13 22:55:34 +0300

    Revert "try fix deploy-tile to use correct config file from ENV var"
    This reverts commit 850bc248560ac0c452c29604d4754e9e134ef24f.

--- ci/tasks/deploy-tile/run.sh
+++ ci/tasks/deploy-tile/run.sh
@@ -49,8 +49,7 @@ ${om} available-products
 ${om} stage-product --product-name "$PRODUCT" --product-version "$VERSION"
 ${om} stage-product --product-name cf --product-version "$cf_version"
 
-echo "CONFIG=${CONFIG}"
-config_path=$base/ops-manager-cloudfoundry/ci/tasks/deploy-tile/config
+config_path=$base/ops-manager-cloudfoundry/ci/tasks/deploy-tile/config.pie
 make_env_config $config_path
 export OM_API_USER=$(yq r $config_path product-properties[.properties.username].value)
 export OM_API_KEY=$(yq r $config_path product-properties[.properties.api_key].value.secret)
