c096829f829847298ad711a3c0583f30ac9a963a
Leo <5437289+cross44@users.noreply.github.com>
2020-01-24 12:36:11 +0300

    install func

--- ci/tasks/deploy-tile/run.sh
+++ ci/tasks/deploy-tile/run.sh
@@ -3,11 +3,6 @@ set -euo pipefail
 [[ ${DEBUG:-} == true ]] && set -x
 
 base=$PWD
-PCF_URL="$PCF_URL"
-PCF_USERNAME="$PCF_USERNAME"
-PCF_PASSWORD="$PCF_PASSWORD"
-UPDATE_PAS="$UPDATE_PAS"
-
 . "$base/ops-manager-cloudfoundry/ci/tasks/helpers/tmp-helper.sh"
 
 VERSION=$(cat "$base"/version/number)
@@ -26,49 +21,57 @@ if [ -z "${TILE_FILE}" ]; then
 	exit 1
 fi
 
-STEMCELL_FILE=$(
-	cd stemcell
-	ls *bosh-stemcell-*.tgz
-)
-if [ -z "${STEMCELL_FILE}" ]; then
-	echo "No files matching stemcell/*.tgz"
-	ls -lR stemcell
-	exit 1
-fi
+install_product $VERSION $TILE_FILE
 
-PRODUCT="$(cat $base/ops-manager-cloudfoundry/tile/tile.yml | grep '^name' | cut -d' ' -f 2)"
-echo "Product " $PRODUCT
-# if [ -z "${PRODUCT}" ]; then
-# 	PRODUCT=mongodb-on-demand
-# fi
+install_product() {
+	VERSION=$1
+	TILE_FILE=$2
+	PCF_URL="$PCF_URL"
+	PCF_USERNAME="$PCF_USERNAME"
+	PCF_PASSWORD="$PCF_PASSWORD"
+	UPDATE_PAS="$UPDATE_PAS"
 
-om="om -t $PCF_URL -u $PCF_USERNAME -p $PCF_PASSWORD -k"
-cf_version=$(${om} available-products -f json | jq -j '.[] | select(.name == "cf").version')
+	STEMCELL_FILE=$(
+		cd stemcell
+		ls *bosh-stemcell-*.tgz
+	)
+	if [ -z "${STEMCELL_FILE}" ]; then
+		echo "No files matching stemcell/*.tgz"
+		ls -lR stemcell
+		exit 1
+	fi
 
-${om} upload-product --product "artifacts/$TILE_FILE"
-${om} upload-stemcell --stemcell "stemcell/$STEMCELL_FILE"
-${om} available-products
-${om} stage-product --product-name "$PRODUCT" --product-version "$VERSION"
-${om} stage-product --product-name cf --product-version "$cf_version"
+	PRODUCT="$(cat $base/ops-manager-cloudfoundry/tile/tile.yml | grep '^name' | cut -d' ' -f 2)"
+	echo "Product " $PRODUCT
 
-config_path=$base/ops-manager-cloudfoundry/ci/tasks/deploy-tile/config.pie
-make_env_config $config_path
-export OM_API_USER=$(yq r $config_path product-properties[.properties.username].value)
-export OM_API_KEY=$(yq r $config_path product-properties[.properties.api_key].value.secret)
-${om} configure-product --config "$config_path" --vars-env OM_API
+	om="om -t $PCF_URL -u $PCF_USERNAME -p $PCF_PASSWORD -k"
+	cf_version=$(${om} available-products -f json | jq -j '.[] | select(.name == "cf").version')
 
-STAGED=$(${om} curl --path /api/v0/staged/products)
-# get GUIDs of cf and $PRODUCT
-if [ "${UPDATE_PAS}" == "true" ]; then
-	RESULT=$(echo "$STAGED" | jq --arg product_name "$PRODUCT" '.[] | select(.type == $product_name or .type == "cf") | .guid')
-else
-	RESULT=$(echo "$STAGED" | jq --arg product_name "$PRODUCT" '.[] | select(.type == $product_name) | .guid')
-fi
+	${om} upload-product --product "artifacts/$TILE_FILE"
+	${om} upload-stemcell --stemcell "stemcell/$STEMCELL_FILE"
+	${om} available-products
+	${om} stage-product --product-name "$PRODUCT" --product-version "$VERSION"
+	${om} stage-product --product-name cf --product-version "$cf_version"
 
-# merge GUIDs
-RESULT=$(echo "$RESULT" | jq --slurp)
-DATA=$(echo '{"deploy_products": []}' | jq ".deploy_products += $RESULT")
+	config_path=$base/ops-manager-cloudfoundry/ci/tasks/deploy-tile/config.pie
+	make_env_config $config_path
+	export OM_API_USER=$(yq r $config_path product-properties[.properties.username].value)
+	export OM_API_KEY=$(yq r $config_path product-properties[.properties.api_key].value.secret)
+	${om} configure-product --config "$config_path" --vars-env OM_API
 
-${om} curl --path /api/v0/installations --request POST --data "$DATA"
-${om} apply-changes --reattach -n cf,"$PRODUCT"
-${om} delete-unused-products
+	STAGED=$(${om} curl --path /api/v0/staged/products)
+	# get GUIDs of cf and $PRODUCT
+	if [ "${UPDATE_PAS}" == "true" ]; then
+		RESULT=$(echo "$STAGED" | jq --arg product_name "$PRODUCT" '.[] | select(.type == $product_name or .type == "cf") | .guid')
+	else
+		RESULT=$(echo "$STAGED" | jq --arg product_name "$PRODUCT" '.[] | select(.type == $product_name) | .guid')
+	fi
+
+	# merge GUIDs
+	RESULT=$(echo "$RESULT" | jq --slurp)
+	DATA=$(echo '{"deploy_products": []}' | jq ".deploy_products += $RESULT")
+
+	${om} curl --path /api/v0/installations --request POST --data "$DATA"
+	${om} apply-changes --reattach -n cf,"$PRODUCT"
+	${om} delete-unused-products
+}
